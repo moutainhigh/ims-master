b72b96b045b8d4f577eadde058ef35a788a9c944
mrsoa <mrduan@mrsoa.com>
2020-02-18 11:02:50 +0800

    second commit

--- ims-web/src/pages/Show.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import HelloWorld from './HelloWorld' //导入HelloWorld组件
-class Show extends React.Component{
-    render(){
-        return <HelloWorld name="zhangsan">shanghai</HelloWorld>; //使用HelloWorld组件
-    }
-}
-export default Show
